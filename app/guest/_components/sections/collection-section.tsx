a3cb18495e695471ed544c4803e16d543ebbb8cd
Yoyan <yoyan947@gmail.com>
2025-09-06 18:29:22 +0800

    Refactor code structure for improved readability and maintainability

--- app/guest/_components/sections/collection-section.tsx
+++ app/guest/_components/sections/collection-section.tsx
@@ -1,26 +1,50 @@
 "use client";
-import { Card, CardBody, Image } from "@heroui/react";
+import React from "react";
+import { Room } from "@/types/room";
+import { Button, Card, CardBody, Image, Spinner } from "@heroui/react";
+import { ArrowUpRight } from "lucide-react";
+import { formatPHP } from "@/lib/format-php";
 
-const rooms = [
-  { name: "Deluxe Room", price: "$300/Night", img: "/deluxe.jpg" },
-  { name: "Standard Room", price: "$200/Night", img: "/standard.jpg" },
-  { name: "Superior Room", price: "$350/Night", img: "/superior.jpg" },
-];
-
-export default function RoomsCarousel() {
-  return (
-    <section className="grid grid-cols-1 sm:grid-cols-3 gap-6 max-w-6xl mx-auto py-12">
-      {rooms.map((room, i) => (
-        <Card key={i} isHoverable>
-          <CardBody>
-            <Image src={room.img} alt={room.name} className="rounded-lg" />
-            <div className="mt-4 text-center">
-              <p className="font-semibold">{room.name}</p>
-              <p className="text-gray-500">{room.price}</p>
-            </div>
-          </CardBody>
-        </Card>
-      ))}
-    </section>
-  );
+interface RoomProps {
+  rooms: Room[];
+  isLoading: boolean;
 }
+export const RoomsCarousel: React.FC<RoomProps> = ({ rooms, isLoading }) => {
+  return (
+    <div>
+      {!isLoading ? (
+        <section className="grid grid-cols-1 sm:grid-cols-3 gap-6 max-w-6xl mx-auto py-12">
+          {rooms.map((room: Room) => (
+            <Card key={room.id} isHoverable>
+              <CardBody>
+                <Image
+                  src="/bg.jpg"
+                  alt={room.room_type}
+                  className="rounded-lg"
+                />
+                <div className="mt-4 flex flex-col h-full gap-4 justify-between">
+                  <div>
+                    <p className="font-semibold">{room.room_type}</p>
+                    <p className="font-light text-gray-500">
+                      {room.description}
+                    </p>
+                  </div>
+                  <div className="flex justify-between items-center">
+                    <p className="text-gray-700">
+                      {formatPHP(Number(room.base_price))}
+                    </p>
+                    <Button color="primary">
+                      <ArrowUpRight />
+                    </Button>
+                  </div>
+                </div>
+              </CardBody>
+            </Card>
+          ))}
+        </section>
+      ) : (
+        <Spinner label="Loading..." />
+      )}
+    </div>
+  );
+};
--- app/guest/page.tsx
+++ app/guest/page.tsx
@@ -1,17 +1,29 @@
 "use client";
 
+import { useDispatch, useSelector } from "react-redux";
 import About from "./_components/sections/about-section";
-import RoomsCarousel from "./_components/sections/collection-section";
+import { RoomsCarousel } from "./_components/sections/collection-section";
 import HeroBanner from "./_components/sections/hero-section";
 import Stats from "./_components/sections/stat-section";
+import { AppDispatch, RootState } from "@/store/store";
+import React from "react";
+import { fetchRooms } from "@/features/room/room-thunk";
 
 export default function page() {
+  const { rooms, isLoading, error } = useSelector(
+    (state: RootState) => state.room
+  );
+  const dispatch = useDispatch<AppDispatch>();
+
+  React.useEffect(() => {
+    dispatch(fetchRooms());
+  }, [dispatch]);
   return (
     <div>
       <HeroBanner />
       <About />
       <Stats />
-      <RoomsCarousel />
+      <RoomsCarousel rooms={rooms} isLoading={isLoading} />
     </div>
   );
 }
